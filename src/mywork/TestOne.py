0f34d1c13ac5c9634d84634baea74db8d43b8b48
Charlie Alhusen <calhusen@gmail.com>
2022-08-30 20:28:56 +0000

    pet solver

--- src/mywork/TestOne.py
+++ src/mywork/TestOne.py
@@ -2,6 +2,8 @@ from z3 import *
 
 
 
-x = Real('x')
-y = Real('y')
-solve(x**2 + y**2 > 3, x**3 + y < 5)
+x = Int('x')
+y = Int('y')
+z = Int('z')
+
+solve(15*x + y + .25 * z == 100, x + y + z == 100, x > 0, y > 0, z > 0)
