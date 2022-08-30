4349a9669da9c5f3744f01854505c9c959e475f9
Charlie Alhusen <calhusen@gmail.com>
2022-08-30 20:06:53 +0000

    testing more

--- src/mywork/TestOne.py
+++ src/mywork/TestOne.py
@@ -1,8 +1,7 @@
 from z3 import *
 
 
-x = Int('x')
-y = Int('y')
-print(simplify(x+y+2*x+3))
-print (simplify(x < y + x + 2))
-print (simplify(And(x + 1 >= 3, x**2 + x**2 + y**2 + 2 >= 5)))
+
+x = Real('x')
+y = Real('y')
+solve(x**2 + y**2 > 3, x**3 + y < 5)
